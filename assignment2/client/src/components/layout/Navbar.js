c8c099baa47f5ae19708ee5b0fa3d65e3c5bec75
a6rivas8 <alexander.rivas@live.com>
2019-07-23 21:09:27 -0500

    Added components/auth folder and set up Register and Login forms. Landing page buttons direct to correct routes. Register form inserts new users successfully.

--- assignment2/client/src/components/layout/Navbar.js
+++ assignment2/client/src/components/layout/Navbar.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className='navbar bg-dark'>
             <h1>
-                <a href='index.html'>
+                <Link to='/'>
                     {' '}
                     <i className='fas fa-car' /> Fuel Picker{' '}
-                </a>
+                </Link>
             </h1>
             <ul>
                 <li>
-                    <a href='register.html'>Register</a>
+                    <Link to='/register'>Register</Link>
                 </li>
                 <li>
-                    <a href='login.html'>Login</a>
+                    <Link to='/login'>Login</Link>
                 </li>
             </ul>
         </nav>
