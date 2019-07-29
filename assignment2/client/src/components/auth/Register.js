6b87fb0bab2dcdf6d412e9361b4a4a45f9b4d375
a6rivas8 <alexander.rivas@live.com>
2019-07-28 19:53:03 -0500

    Redux Alerts set up and setting user authentication. Registering works.

--- assignment2/client/src/components/auth/Register.js
+++ assignment2/client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -22,7 +23,7 @@ const Register = ({ setAlert }) => {
         if (password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('SUCCESS');
+            register({ name, email, password });
 
             /* -- code to see how request is made
                -- redux will do this
@@ -63,7 +64,6 @@ const Register = ({ setAlert }) => {
                         name='name'
                         value={name}
                         onChange={e => onChange(e)}
-                        required
                     />
                 </div>
                 <div className='form-group'>
@@ -73,7 +73,6 @@ const Register = ({ setAlert }) => {
                         name='email'
                         value={email}
                         onChange={e => onChange(e)}
-                        required
                     />
                     <small className='form-text'>
                         This site uses Gravatar, so if you would like a profile
@@ -88,7 +87,6 @@ const Register = ({ setAlert }) => {
                         minLength='8'
                         value={password}
                         onChange={e => onChange(e)}
-                        required
                     />
                 </div>
                 <div className='form-group'>
@@ -99,7 +97,6 @@ const Register = ({ setAlert }) => {
                         name='password2'
                         value={password2}
                         onChange={e => onChange(e)}
-                        required
                     />
                 </div>
                 <input
@@ -116,10 +113,11 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
 };
 
 export default connect(
     null,
-    { setAlert }
+    { setAlert, register }
 )(Register);
