5ee6debfde1ad53fc5648bc4c49be37fb4b3e49b
Conrad Parker <conradlparker@gmail.com>
2019-08-04 22:55:52 -0500

    fixed nav bar for users without profiles

--- assignment2/client/src/components/layout/Navbar.js
+++ assignment2/client/src/components/layout/Navbar.js
@@ -3,8 +3,31 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+const Navbar = ({
+    getCurrentProfile,
+    profile,
+    auth: { isAuthenticated, loading },
+    logout
+}) => {
+    const authLinkNoProfile = (
+        <ul>
+            <li>
+                <Link to='/dashboard'>
+                    <i className='fas fa-user' />{' '}
+                    <span className='hide-sm'> Dashboard</span>
+                </Link>
+            </li>
+            <li>
+                <a onClick={logout} href='#!'>
+                    <i className='fas fa-sign-out-alt' />
+                    <span className='hide-sm'> Logout</span>
+                </a>
+            </li>
+        </ul>
+    );
+
     const authLinks = (
         <ul>
             <li>
@@ -47,14 +70,19 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
                     <i className='fas fa-car' /> Fuel Picker{' '}
                 </Link>
             </h1>
-            {!loading && (
-                <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+            {!loading && isAuthenticated && (
+                <Fragment>
+                    {profile !== null ? authLinkNoProfile : authLinks}
+                </Fragment>
             )}
+            {!loading && !isAuthenticated && <Fragment>{guestLinks}</Fragment>}
         </nav>
     );
 };
 
 Navbar.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
     logout: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
 };
@@ -65,5 +93,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { logout }
+    { getCurrentProfile, logout }
 )(Navbar);
